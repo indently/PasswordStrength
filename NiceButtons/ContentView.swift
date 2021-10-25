8de9120380c75b094b0a005ebcad116e697f63fc
Code Palace <codepalace@fastmail.se>
2021-10-25 08:30:45 +0200

    Initial Commit

---
+++ NiceButtons/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  NiceButtons
+//
+//  Created by Federico on 24/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var password : String = ""
+    @State var passwordStrength : Int = 0
+    
+    func checkStrength(_ password: String) -> Int {
+        let passwordLength = password.count
+        var containsSymbol = false
+        var containsUppercase = false 
+
+        for character in password {
+            if "ABCDEFGHIJKLMNOPQRSTUVWXYZ".contains(character) {
+                containsUppercase = true
+            }
+            
+            if "!£$%&/()=?^;:_ç°§*,.-_".contains(character) {
+                containsSymbol = true
+            }
+        }
+        
+        if passwordLength > 8 && containsSymbol && containsUppercase {
+            return 1
+        } else {
+            return 0
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("How strong is your password?")
+                .padding()
+            
+            TextField("Enter your password:", text: $password).textFieldStyle(.roundedBorder)
+                .frame(width: 250)
+            
+            HStack {
+                if checkStrength(password) == 0  {
+                    Text("Weak").foregroundColor(Color.red)
+                        .font(.system(size: 30.0)).padding()
+                } else {
+                    Text("Strong").foregroundColor(Color.green)
+                        .font(.system(size: 30.0)).padding()
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NiceButtons/NiceButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceButtonsApp.swift
+//  NiceButtons
+//
+//  Created by Federico on 24/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NiceButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
